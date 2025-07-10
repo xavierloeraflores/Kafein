cc42d64de59d312e5874eca3de6199d9118d7655
xavierloeraflores <x.loera.tech@gmail.com>
2025-07-10 12:42:13 -0700

    Add functions to convert drink IDs, calculate prices, and generate drink names, enhancing order processing and user experience.

---
+++ web/src/lib/convertdrinkId.ts
@@ -0,0 +1,47 @@
+import { products } from "./constants";
+
+export function convertDrinkId(drinkId: string) {
+  const [drink, milkType, sweetness, size, matchaShots] = drinkId.split("-");
+  return {
+    drink,
+    milkType,
+    sweetness,
+    size,
+    matchaShots,
+  };
+}
+
+export function getDrinkPrice(drinkId: string) {
+  const { drink, milkType, size, matchaShots } = convertDrinkId(drinkId);
+  let drinkPrice = products.find((product) => product.id === drink)?.price;
+  if (!drinkPrice) {
+    throw new Error("Drink not found");
+  }
+  if (size === "large") {
+    drinkPrice += 1;
+  }
+  if (matchaShots && Number(matchaShots) > 0) {
+    drinkPrice += 0.5 * Number(matchaShots);
+  }
+  if (milkType === "whole") {
+    drinkPrice += 1;
+  }
+  return drinkPrice;
+}
+
+export function getDrinkName(drinkId: string) {
+  const { drink, milkType, sweetness, size, matchaShots } =
+    convertDrinkId(drinkId);
+  const drinkNameLabel = products.find(
+    (product) => product.id === drink,
+  )?.title;
+  if (!drinkNameLabel) {
+    throw new Error("Drink not found");
+  }
+  const milkTypeLabel = milkType === "oat" ? "Oat Milk" : "Whole Milk";
+  const sweetnessLabel = sweetness === "regular" ? "" : "Extra Sweet";
+  const sizeLabel = size === "regular" ? "12oz" : "16oz";
+  const matchaShotsLabel =
+    matchaShots && Number(matchaShots) > 0 ? `+${matchaShots} Matcha Shot` : "";
+  return `${drinkNameLabel} ${milkTypeLabel} ${sweetnessLabel} ${sizeLabel} ${matchaShotsLabel}`;
+}
