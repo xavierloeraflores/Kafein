4ad048b75b4573da9b4decc39e899731c84b15aa
xavierloeraflores <x.loera.tech@gmail.com>
2025-08-29 18:05:32 -0700

    Add db.ts file to initialize Prisma client with environment-specific logging and global instance management for database interactions.

---
+++ web/src/server/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { env } from "~/env";
+
+const createPrismaClient = () =>
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  }).$extends(withAccelerate());
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
