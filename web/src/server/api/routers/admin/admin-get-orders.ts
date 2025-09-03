677eeef5b79ddf17a25b035485104f583f4be86c
xavierloeraflores <x.loera.tech@gmail.com>
2025-09-02 23:43:45 -0700

    Refactor adminGetOrdersProcedure to accept pagination input and retrieve orders from the database, enhancing order management functionality.

--- web/src/server/api/routers/admin/admin-get-orders.ts
+++ web/src/server/api/routers/admin/admin-get-orders.ts
@@ -1,10 +1,18 @@
 import { publicProcedure } from "~/server/api/trpc";
-import { orderSchema } from "~/lib/schemas";
-
+import { db } from "~/server/db";
+import { z } from "zod";
 export const adminGetOrdersProcedure = publicProcedure
-  .input(orderSchema)
-  .mutation(async ({ input }) => {
-    return "order";
+  .input(z.object({ page: z.number() }))
+  .query(async ({ input }) => {
+    const { page } = input;
+    const orders = await db.order.findMany({
+      skip: (page - 1) * 10,
+      take: 10,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return orders;
   });
 
 export default adminGetOrdersProcedure;
